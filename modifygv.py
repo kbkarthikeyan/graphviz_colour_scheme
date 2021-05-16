432523e5505df81ea371e74fea6500bf350ec50f
kbkarthikeyan <kbkarthikeyan@gmail.com>
2021-05-17 01:23:30 +0200

    Update modifygv.py

--- modifygv.py
+++ modifygv.py
@@ -10,17 +10,17 @@ def load_file(file_name): #to load a file for viewing or modifying
     file_alter = graphviz.Source.from_file(file_name)
     return file_alter
 
-def modi_sent(stri_name, node): # to modify the gv file and add colour map
+def set_map(stri_name, node): # to modify the gv file and insert colour map for eg.. set_map(x,y), x is gvfile and y is heatmap for nodes!
     tmp = stri_name.source
     tmp = tmp.split(";")
     tmp[0] = S0
     tmp[1] = S1
     for i in range(3, len(node)):
-        tmp[i] = "{} [{} = {}]".format(tmp[i],"color", int(node[i]/20))
+        tmp[i+3] = "{} [{} = {}]".format(tmp[i+3],"color", int(node[i]/20))
     new_sent = ";".join(map(str,tmp))
     return new_sent
     
-def file_save(file_name, stri_name): # to save the modified file to gv
+def save_file(file_name, stri_name): # to save the modified file to gv
     with open(file_name, "w") as out:
         out.write(stri_name)
         
@@ -32,6 +32,6 @@ def view(stri_name):  #to view the file in neato in the png format
     
 def main(file_name, node, file_sn):
     a = load_file(file_name)
-    b = modi_sent(a, node)
-    file_save(file_sn, b)
+    b = set_map(a, node)
+    save_file(file_sn, b)
     
